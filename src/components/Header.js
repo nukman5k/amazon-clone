b24dfe05be8dc9cb3d1fd5510ac67932e60d7972
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-03-14 17:58:46 +0200

    search bar design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
+import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
 
 const Header = () => {
     return (
         <header>
             {/* Top nav */}
             <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
-                <div className='mt-2 flex items-center'>
+                <div className='mt-2 flex items-center flex-grow sm:flex-grow-0 '>
                     <Image
                         src='https://links.papareact.com/f90'
                         width={150}
@@ -15,12 +16,20 @@ const Header = () => {
                         className='cursor-pointer'
                     />
                 </div>
+
+                {/* SEARCH BAR */}
+                <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
+                    <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' 
+                    type="text" />
+                    <SearchIcon className='h-12 p-4' />
+                </div>
             </div>
 
             {/* Bottom nav */}
             <div>
 
             </div>
+
         </header>
     )
 }
