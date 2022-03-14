69f4da55c8292243c1a68b65b83334ff098aebd9
Ndabenhle Kunene <ndakunene@outlook.com>
2022-03-14 15:07:32 +0200

    14marchUpdate

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header>
+            {/* Top nav */}
+            <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+                <div className='mt-2 flex items-center'>
+                    <Image
+                        src='https://links.papareact.com/f90'
+                        width={150}
+                        height={40}
+                        objectFit='contain'
+                        className='cursor-pointer'
+                    />
+                </div>
+            </div>
+
+            {/* Bottom nav */}
+            <div>
+
+            </div>
+        </header>
+    )
 }
 
-export default Header
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,9 +8,9 @@ export default function Home() {
         <title>Amazon 2.0</title>
       </Head>
 
-      <h1>NDA</h1>
       {/* Header */}
- 
+      <Header />
+
     </div>
   );
 }
