2fd0632b899ada547c7160f58459e69e9d67b56e
Ndabenhle Kunene <46968027+nukman5k@users.noreply.github.com>
2022-03-15 09:09:31 +0200

    14-march-update

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,10 +19,32 @@ const Header = () => {
 
                 {/* SEARCH BAR */}
                 <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
-                    <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' 
-                    type="text" />
+                    <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4'
+                        type="text" />
                     <SearchIcon className='h-12 p-4' />
                 </div>
+
+                {/* RIght-side */}
+                <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+                    {/* SIGN-IN NAME */}
+                    <div className='link'>
+                        <p>HELLO NDA</p>
+                        <p>Account & Lists</p>
+                    </div>
+
+                    {/* RETURNS & ORDERS */}
+                    <div className='link'>
+                        <p>Returns</p>
+                        <p>& Orders</p>
+                    </div>
+
+                    {/* BASKET */}
+                    <div className='link'>
+                        <ShoppingCartIcon className='h-10' />
+                        <p>Basket</p>
+                    </div>
+                </div>
+
             </div>
 
             {/* Bottom nav */}
